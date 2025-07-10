595c581afcf1baefe1a3a4ffb57fb614d02201e5
Awortwe Daniel <awortwedanieljr15@gmail.com>
2025-07-10 20:34:04 +0000

    data types in c#

---
+++ learning_c#_fromTheBasics/dataTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("DataTypes\n=================================\n");
+
+        /*
+         * The dataTypes in c#  include; 
+         * int for numbers
+         * double for decimals
+         * string for sequence of characters
+         * char for a single character
+         * bool for true or false
+         */
+
+        int age = 12;
+        double shoeSize = 39.05;
+        string stuName = "Geniel";
+        char det = 'a';
+        bool isGood = true;
+
+        Console.WriteLine(stuName + ", " + det + " good lady, is " + age + "years old.");
+    }
+}
