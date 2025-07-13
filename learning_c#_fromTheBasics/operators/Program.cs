26970ec73c17c23c36b2d73f13b7ca284f69d989
Awortwe Daniel <awortwedanieljr15@gmail.com>
2025-07-13 23:40:38 +0000

    Working with operators in c#

---
+++ learning_c#_fromTheBasics/operators/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Operators");
+
+        /*
+         * '+' operator adds to integers together or calculates the sum of two intergers or doubles
+         
+         */
+
+        Console.WriteLine("Enter first number:\n");
+        int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:\n");
+        int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+        int sum = firstnumber + secondnumber;
+        Console.WriteLine(sum);
+
+
+    }
+}
