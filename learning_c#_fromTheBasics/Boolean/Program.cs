02fcf3cdf2758e93fc3f19b69c363956e9807f94
Awortwe Daniel <awortwedanieljr15@gmail.com>
2025-07-17 23:03:15 +0000

    boolean variable in c#

---
+++ learning_c#_fromTheBasics/Boolean/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Booleans-True or False");
+        /*
+         * bool is a data type that can values true or false
+         * Declaring boolean variables, the syntax is `bool` keyword + variable name
+         * Examples are below;
+         */
+
+        bool isABoy = true;
+        bool isMyFriend = false;
+        bool isGood = false;
+        Console.WriteLine(isABoy);
+        Console.WriteLine(isGood);
+        Console.WriteLine("Enter true or false");
+        string newk = Console.ReadLine();
+        Console.WriteLine(newk);
+
+        Console.WriteLine(newk == Convert.ToString(isGood));
+
+
+    }
+}
