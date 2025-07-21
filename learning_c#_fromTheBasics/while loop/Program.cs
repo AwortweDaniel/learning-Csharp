ad0794347675c8c2721353465e454958bbe22b7c
Awortwe Daniel <awortwedanieljr15@gmail.com>
2025-07-21 17:55:45 +0000

    while and do-while loop in c3

--- learning_c#_fromTheBasics/while loop/Program.cs
+++ learning_c#_fromTheBasics/while loop/Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine(" loops - for and while");
+        Console.WriteLine("While loop");
         /*
          * Loops are codes or can execute a block of codes so far as a condition is met
          * Loops are handy because they save time, reduce more error and makes code more readable
@@ -26,6 +26,25 @@
             i++;
         }
 
+        /*
+         * Another variant of the while loop is the do while loop
+         * for this as the name suggests, a block of codes is executed first,
+         * before checking if the condition is true, and then repeat until the condition changes to False
+         * 
+         * Syntax for do while loop is below;
+         * do
+         * {
+         *     execute task
+         * }while(condition);
+         * 
+         * An example is below
+         */
 
+        int j = 0;
+        do
+        {
+            Console.WriteLine(j);
+            j++;
+        } while (j < 10);
     }
 }
